6e139611666b7a7f3fc0a7023372eaf7d05c8822
hiroyuki19841103-cmyk <hiroyuki19841103@gmail.com>
2026-09-12 20:11:10 +0900

    Microsoft認証設定を追加

--- src/app-config.js
+++ src/app-config.js
@@ -1,9 +1,9 @@
 /* Microsoft Entraの既存TASK CONTROLアプリと同じCLIENT_IDを使用します。 */
 window.BUCHO_TODAY_CONFIG = Object.freeze({
   APP_VERSION: "2026.09.12-v1.0",
-  CLIENT_ID: "YOUR_CLIENT_ID_HERE",
+  CLIENT_ID: "421c060c-f769-4d5b-9bab-8c3b9a4cf3ec",
   AUTHORITY: "https://login.microsoftonline.com/common/",
-  REDIRECT_URI: "https://YOUR-SITE.azurestaticapps.net/",
+  REDIRECT_URI: "https://witty-mushroom-0fcb9cb10.3.azurestaticapps.net/",
   GRAPH_SCOPES: ["Files.ReadWrite.AppFolder", "Calendars.ReadWrite"],
   TASK_FILE_NAME: "TASK_CONTROL_Master.xlsx",
   TODAY_FILE_NAME: "BUCHO_TODAY_Master.xlsx",
